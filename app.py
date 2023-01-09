885f69bd67ec4c7c391bfd0343fa299dda485fae
GrantPerkins <gcperk20@gmail.com>
2023-01-09 12:19:17 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+from dao import DAO
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Bonjour world! from FaceBeer API"
+
+
+@app.route("/get_leaderboard", methods=["GET"])
+def test_pull():
+    rows = dao.get_all_rows()
+    response = {"code": 500, "message": rows}
+    return response
+
+
+@app.route("/append", methods=["POST"])
+def add_reading():
+    name = request.form["name"]
+    bac = request.form["bac"]
+    timestamp = request.form["timestamp"]
+    dao.add_item(name, bac, timestamp)
+    response = {"code": 500, "message": "Successfully added row"}
+    return response
+
+
+if __name__ == "__main__":
+    dao = DAO()
+    app.run(host='0.0.0.0', port=8000)
